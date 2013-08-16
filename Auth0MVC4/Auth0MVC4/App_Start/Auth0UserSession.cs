93a4e300684f09080371671a293c5f859e524713
Rasika Sherman <rasika.sherman@gmail.com>
2013-08-16 12:05:54 +1000

    Auth0 ServiceStack test

---
+++ Auth0MVC4/Auth0MVC4/App_Start/Auth0UserSession.cs
@@ -0,0 +1,19 @@
+namespace Auth0MVC4.App_Start
+{
+	using ServiceStack.ServiceInterface.Auth;
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A simple custom User Session that stores all simple properties.
+    /// </summary>
+    public class Auth0UserSession : AuthUserSession
+    {
+        public Auth0UserSession()
+        {
+            this.ExtraData = new Dictionary<string, string>();
+        }
+
+        public Dictionary<string, string> ExtraData { get; set; }
+    }
+}
